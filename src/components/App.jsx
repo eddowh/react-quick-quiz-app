adb377deaa8ab331d0332698d916c25f4950be09
Eddo Hintoso <eddo.hintoso@gmail.com>
2016-04-13 12:42:51 +0800

    Complete the main components for question layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
+import QuestionList from './quiz/QuestionList.jsx';
+
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+            // questions array:
+            // each objects represent a question
+            // see 'data/questions.json'
+            questions: require('json!' + '../../data/questions.json'),
+
+            // score will later be calculated as a percentage
+            score: 0,
+            current: 1
+        }
+    }
 
     render() {
         return (
             <div>
-                React Quick Quiz App
+                <QuestionList {...this.state} />
             </div>
         )
     }
